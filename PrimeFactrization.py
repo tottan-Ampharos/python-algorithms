a11b5713a3f16d6d5755555d579bb0045ea288f7
dike-okayama <lawliet.d.k.m@gmail.com>
2023-03-06 23:18:37 +0900

    wrote BETTER code

--- PrimeFactrization.py
+++ PrimeFactrization.py
@@ -1,6 +1,29 @@
-def PrimeFactorization(test):
-    return test
+# -*- coding: utf-8 -*-
 
 
-n = input()
-print(PrimeFactorization(n))
+def prime_factorization(n: int) -> dict[int, int]:
+    '''
+    Factorize a natural number n.
+    >>> prime_factorization(1991)
+    {11: 1, 181: 1}
+    >>> prime_factorization(25)
+    {5: 2}
+    '''
+    assert isinstance(n, int)
+    assert n > 1
+
+    res: dict[int, int] = {}
+    for i in range(2, int(n ** .5) + 1):
+        while n % i == 0:
+            res[i] = res.get(i, 0) + 1
+            n //= i
+    if n != 1:
+        res[n] = 1
+    return res
+
+
+if __name__ == '__main__':
+
+    import doctest
+
+    doctest.run_docstring_examples(prime_factorization, globals())
